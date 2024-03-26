c978d7d970b1700971e938b4b5f8ff9d040e1091
Sid Rastogi <sidrastogi9015@gmail.com>
2024-03-26 12:21:13 +0530

    Add User with interval

--- create-update-contact.js
+++ create-update-contact.js
@@ -3,7 +3,7 @@ exports.sendEmail = async (client) => {
     const data = {
       contacts: [
         {
-          email: "roorkee444@gmail.com",
+          email: "sid1@gmail.com",
         },
       ],
     };
@@ -13,8 +13,35 @@ exports.sendEmail = async (client) => {
       body: data,
     };
     const response = await client.request(request);
-    console.log(response);
+    const { statusCode = 0, body: { job_id = null } = {} } = response[0];
+    console.log(job_id, "job_id");
+    if (statusCode === 202 && job_id) {
+      const getJobStatusInterval = setInterval(async () => {
+        const statusResponse = await getJobStatus(client, job_id);
+        const { statusCode = 0, body: { status = null } = {} } =
+          statusResponse[0];
+        if (statusCode === 200 && status !== "pending") {
+          console.log(statusResponse[0], "Done");
+          clearInterval(getJobStatusInterval);
+          return true;
+        }
+        console.log(statusResponse[0], "pending");
+        return true;
+      }, 5000);
+    }
   } catch (error) {
     console.log(JSON.stringify(error, null, 2));
+  }
+};
+
+const getJobStatus = async (client, id) => {
+  try {
+    const request = {
+      url: `/v3/marketing/contacts/imports/${id}`,
+      method: "GET",
+    };
+    return await client.request(request);
+  } catch (error) {
+    console.error(error);
   }
 };
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const client = require('@sendgrid/client');
-const { sendEmail } = require('./create-update-contact');
-const { checkImportContactStatus } = require('./check-import-contact-status');
+const client = require("@sendgrid/client");
+const { sendEmail } = require("./create-update-contact");
 
 client.setApiKey(process.env.SENDGRID_API_KEY);
 
 console.log(process.env.SENDGRID_API_KEY);
 
-// sendEmail(client);
-checkImportContactStatus(client);
+sendEmail(client);
+// checkImportContactStatus(client);
